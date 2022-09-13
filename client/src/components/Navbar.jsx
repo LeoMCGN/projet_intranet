44d8e8b8e8c7a99268c31aa2e22a2fe2b42e7b3c
LEO MACAIGNE <leo.macaigne@gmail.com>
2022-09-13 09:21:03 +0200

    First commit

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,24 +5,27 @@ function Navbar() {
   const [count, setCount] = useState(0);
 
   return (
-    <nav>
-      <div class="nav-header">
-        <div class="nav-title">INTRANET</div>
-      </div>
-      <div class="nav-btn">
-        <label for="nav-check">
-          <span></span>
-          <span></span>
-          <span></span>
-        </label>
-      </div>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Accueil</a>
+          </li>
+          <li>
+            <a href="#">Login</a>
+          </li>
+        </ul>
+      </nav>
 
-      <div class="nav-links">
-        <a href="#" target="_blank" className="active">
-          LOGIN
-        </a>
-      </div>
-    </nav>
+      <section>
+        <div className="two alt-two">
+          <h1>
+            Intranet
+            <span>Bienvenue sur l'intranet</span>
+          </h1>
+        </div>
+      </section>
+    </div>
   );
 }
 
